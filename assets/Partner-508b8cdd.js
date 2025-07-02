69313bbd617d77dd01e17d3011f999dac9719309
林林林枫 <10249415+lin-lin-lin-feng@user.noreply.gitee.com>
2025-07-02 17:13:01 +0800

    router change_3

---
+++ assets/Partner-508b8cdd.js
@@ -0,0 +1,1 @@
+import{P as e}from"./PageBase-8681af4f.js";import{b as r,k as t}from"./index-9c6be662.js";const n={__name:"Partner",setup(a){return(o,s)=>(r(),t(e,{title:"合作伙伴"}))}};export{n as default};
--- assets/Partner-70e0d342.js
+++
@@ -1,1 +0,0 @@
-import{P as e}from"./PageBase-5e228054.js";import{b as r,k as t}from"./index-3668387d.js";const n={__name:"Partner",setup(a){return(o,s)=>(r(),t(e,{title:"合作伙伴"}))}};export{n as default};
