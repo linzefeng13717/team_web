174c8eaa538f4632d4ad3107d359e5c319cee961
林林林枫 <10249415+lin-lin-lin-feng@user.noreply.gitee.com>
2025-07-02 17:24:06 +0800

    router change_4

---
+++ assets/Partner-b3e0f21e.js
@@ -0,0 +1,1 @@
+import{P as e}from"./PageBase-b2d79527.js";import{b as r,k as t}from"./index-bd62ee1d.js";const n={__name:"Partner",setup(a){return(o,s)=>(r(),t(e,{title:"合作伙伴"}))}};export{n as default};
